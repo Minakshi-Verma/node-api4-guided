c933af5e6e0ad7c71bcfc73ece10d160c9ca671f
Minakshi Verma <mvedwan@gmail.com>
2020-03-12 13:01:21 -0400

    "changes are amde to db.js"

--- data/db.js
+++ data/db.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 
-const environment = process.env.NODE_ENV || 'development';
+const environment = process.env.DB_ENV || 'development'; // changed node to DB
 const config = require('../knexfile.js')[environment];
 
 module.exports = knex(config);
