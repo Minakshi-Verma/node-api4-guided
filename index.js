27fec9243ea9890ac497f93d48a652d65d0895b4
Minakshi Verma <mvedwan@gmail.com>
2020-03-12 12:43:23 -0400

    "start script added"

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const server = require('./api/server.js');
+//make the post be assigned by the server
+//heroku will place the .PORT environment value on their server
+const port = process.env.PORT || 4000
 
-const port = 4000;
+// const port = 4000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
+
+//Add astart script to package.json, heroku default run on start script which uses node to run the server(API) and not nodemon
